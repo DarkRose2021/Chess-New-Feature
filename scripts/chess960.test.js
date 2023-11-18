cdf770e8baa8b7d93519c99d47ea3e8ad67f846b
DarkRose2021 <89149647+DarkRose2021@users.noreply.github.com>
2023-11-18 11:36:34 -0700

    forgot to delete some vars

--- scripts/chess960.test.js
+++ scripts/chess960.test.js
@@ -9,41 +9,6 @@ const {
 	isSquareUnderAttack,
 } = require("./chess960");
 
-var WHITE = 0x0;
-var BLACK = 0x8;
-
-var PAWN = 0x01;
-var KNIGHT = 0x02;
-var KING = 0x03;
-var BISHOP = 0x05;
-var ROOK = 0x06;
-var QUEEN = 0x07;
-
-// White pieces have their 4th bit = 0
-var WHITE_PAWN = 0x01;
-var WHITE_KNIGHT = 0x02;
-var WHITE_KING = 0x03;
-var WHITE_BISHOP = 0x05;
-var WHITE_ROOK = 0x06;
-var WHITE_QUEEN = 0x07;
-
-// Black pieces have their 4th bit = 1
-var BLACK_PAWN = 0x09;
-var BLACK_KNIGHT = 0x0a;
-var BLACK_KING = 0x0b;
-var BLACK_BISHOP = 0x0d;
-var BLACK_ROOK = 0x0e;
-var BLACK_QUEEN = 0x0f;
-
-var board = [BLACK_ROOK, BLACK_KNIGHT, BLACK_BISHOP, BLACK_QUEEN, BLACK_KING, BLACK_BISHOP, BLACK_KNIGHT, BLACK_ROOK, 0, 0, 0, 0, 0, 0, 0, 0,
-	BLACK_PAWN, BLACK_PAWN, BLACK_PAWN, BLACK_PAWN, BLACK_PAWN, BLACK_PAWN, BLACK_PAWN, BLACK_PAWN, 0, 0, 0, 0, 0, 0, 0, 0,
-	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-	WHITE_PAWN, WHITE_PAWN, WHITE_PAWN, WHITE_PAWN, WHITE_PAWN, WHITE_PAWN, WHITE_PAWN, WHITE_PAWN, 0, 0, 0, 0, 0, 0, 0, 0,
-	WHITE_ROOK, WHITE_KNIGHT, WHITE_BISHOP, WHITE_QUEEN, WHITE_KING, WHITE_BISHOP, WHITE_KNIGHT, WHITE_ROOK, 0, 0, 0, 0, 0, 0, 0, 0];
-
 describe("getWhiteRowArray", () => {
 	test("should return the correct white row array for a given configuration", () => {
 		const randomConfig = [0, 1, 2, 3, 4, 5, 6, 7];
@@ -82,10 +47,10 @@ describe("shuffleBackRow", () => {
 
 describe("isPseudoLegalTesting", () => {
 	const from = 52; // Example square index
-    const to = 36; // Example square index
-    const currentPlayer = WHITE; // Example player
-    const result = isPseudoLegal(from, to, currentPlayer, board);
-    expect(result).toBe(true);
+	const to = 36; // Example square index
+	const currentPlayer = WHITE; // Example player
+	const result = isPseudoLegal(from, to, currentPlayer, board);
+	expect(result).toBe(true);
 });
 
 describe("makeMove", () => {
